b15109d705567d1441ab0a3f843f16b6c3509ab8
Andrew Yule <andrewgordonyule@gmail.com>
2021-12-09 22:00:43 +0000

    add debug 2 solution

---
+++ intro_lab_questions/debugging_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // This program finds the sum of all multiples of 3 and 5 below 100.
+    // It has an error in logic and a syntax error. Correct both.
+    // This should print 2318 when fixed.
+    // int total = 0;
+    // int i;
+    // for (i++; i < 100; i = 0) {
+    //     if (i%3 == 0 || i%5 == 0) {
+    //         total = total + i;
+    //     }
+    // }
+
+    int total = 0;
+    int i;
+    // incorrect for loop declaration
+    for (i = 0; i < 100; i++) {
+        if (i%3 == 0 || i%5 == 0) {
+            total = total + i;
+        }
+    }
+
+    printf("Sum of all multiples is %d.\n", total);
+    return 0;
+}
