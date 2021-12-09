13ca3f77fd6021d7d8a6466d9e24b5d98961111f
Andrew Yule <andrewgordonyule@gmail.com>
2021-12-09 19:25:08 +0000

    add collatz solution q12 and q13

--- intro_lab_questions/collatz.c
+++ intro_lab_questions/collatz.c
@@ -13,7 +13,29 @@ void collatz(int n) {
     printf("%d\n", n);
 }
 
+// return the length of collatz sequence for given number n
+int collatzLength(int n) {
+
+    int len = 0;
+
+    for(; n > 1; ) {
+        if(n % 2 == 0) {
+            n = n/2;
+        } else {
+            n = 3*n + 1;
+        }
+        len++;
+    }
+    // +1 to account for final 1
+    return len + 1;
+
+}
+
 int main() {
+
+    int max_length = 0;
+    int current_num;
+    int collatz_num;
 
     int num;
     printf("Enter your number:");
@@ -21,6 +43,20 @@ int main() {
 
     printf("The collatz sequence starting from %d is:\n", num);
     collatz(num);
+
+    int sequence_length = collatzLength(5);
+    printf("Collatz sequence length of 5 is: %d\n", sequence_length);
+
+    int i;
+    for(i = 2; i < 101; i++) {
+        current_num = collatzLength(i);
+        if (current_num > max_length) {
+            max_length = current_num;
+            collatz_num = i;
+        }
+    }
+
+    printf("Max sequence length is %d with number %d", max_length, collatz_num);
 
     return 0;
 }
