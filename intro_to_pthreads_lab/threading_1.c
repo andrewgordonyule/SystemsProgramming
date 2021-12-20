d0608e749c6a52dbda4b025cf16205906e5998c5
Andrew Yule <andrewgordonyule@gmail.com>
2021-12-20 23:38:21 +0000

    add first problem from pthreads lab

---
+++ intro_to_pthreads_lab/threading_1.c
@@ -0,0 +1,29 @@
+/*
+  This code _should_ print all of the characters in "Hello, world!"
+  Modify `entry()` so that our threads print the characters they're passed as arguments.
+*/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_THREADS 14
+
+void * entry(void * arg) {
+  printf("%c\n", arg);
+}
+
+int main() {
+
+    char hw[] = "Hello, world!";
+    pthread_t threads[NUM_THREADS];
+    int i;
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, entry, &hw[i]);
+    }
+
+    // ask why printed values are weird?
+
+    return 0;
+
+}
